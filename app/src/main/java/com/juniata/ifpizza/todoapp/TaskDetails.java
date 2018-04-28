2f8f7670f7dc3bade3bf66653fc1ba9ae9c03b5b
Charley Bein <diamonddynamo96@gmail.com>
2018-04-28 13:28:46 -0400

    All layouts tentatively done Can tap both parts of tasks now

--- app/src/main/java/com/juniata/ifpizza/todoapp/TaskDetails.java
+++ app/src/main/java/com/juniata/ifpizza/todoapp/TaskDetails.java
@@ -6,7 +6,10 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class TaskDetails extends AppCompatActivity {
@@ -27,7 +30,21 @@ public class TaskDetails extends AppCompatActivity {
 
         final TextInputEditText descField = findViewById(R.id.description_field);
         final ListView subTasks = findViewById(R.id.sub_task_list);
+        final Button saveButton = findViewById(R.id.save_task);
 
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_lists_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
     }
 
 }
